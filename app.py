11da520eaa63fad9567b7f656d2361ff844ff668
Ana Laura <ana.machioni@aluno.senai.br>
2026-03-13 11:29:04 -0300

    add: resolvento botao e cadastrar

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
-from model.requisitos import recuperar_requisitos
+from model.requisitos import recuperar_requisitos, update
 from model.requisitos import inserir_dados
+from model.requisitos import excluir_descricao
+
 
 app = Flask (__name__)
 
@@ -16,16 +18,25 @@ def pag_requisitos():
     requisitos = recuperar_requisitos()
     return render_template("requisitos.html", requisitos = requisitos )
 
-@app.route("/cadastrar")
+@app.route("/cadastrar", methods = ["POST"])
 def pag_cadastrar():
     descricao = request.form.get("descricao")
     nivel = request.form.get("nivel")
     valor =  request.form.get("valor")
-    if inserir_dados(descricao, nivel, valor):
-        return redirect("/")
-    else:
-        return "erro ao adicionar dados"
-    
+    inserir_dados(descricao, nivel, valor)
+    return redirect("/requisitos")
+
+@app.route("/requisitos/delete/<codigo>")
+def deletar_registros(codigo):
+    excluir_descricao(codigo)
+    return redirect("/requisitos")
+
+@app.route("/requisitos/status/<codigo>/<status>")
+def status(codigo, status):
+    update(codigo, status)
+    return redirect("/requisitos")
+
+
 # rodando
 if __name__ == "__main__":
     app.run(debug=True)
--- model/requisitos.py
+++ model/requisitos.py
@@ -15,10 +15,46 @@ def inserir_dados(descricao, nivel, valor):
     conexao, cursor = conectar()
 
     cursor.execute("""
-            INSERT INTO tb_requisitos (DESCRICAO, NIVEL, VALOR)
-            VALUES (%s, %s, %s);
-            """, [descricao, nivel, valor])
+            INSERT INTO tb_requisitos (descricao, nivel, valor, situacao)
+            VALUES (%s, %s, %s, "pendente");
+            """, (descricao, nivel, valor))
     
     conexao.commit()
     cursor.close()
+
+
+def excluir_descricao(cod_requisitos:int) -> bool:
+    try:
+        conexao, cursor = conectar()
+
+        cursor.execute("""
+        DELETE FROM tb_requisitos
+        WHERE cod_requisito = %s
+                       
+        """,
+        [cod_requisitos])
+        conexao.commit()
+
+        conexao.close()
+
+        return True
+    
+    except Exception as erro:
+        print(erro)
+        return False
+    
+
+
+    
+def update(cod_requisitos, situacao):
+    conexao, cursor = conectar()
+
+    cursor.execute("""
+                    UPDATE tb_requisitos SET situacao = %s WHERE cod_requisito = %s;
+            """, (situacao, cod_requisitos ))
+    conexao.commit()
+
+    conexao.close()
+
+
     
